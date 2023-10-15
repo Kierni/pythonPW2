368f8a9ceda71eead92eb41c973f5ee113508fb0
Filip <f.kiernozek@wp.pl>
2023-10-15 10:46:07 +0200

    refractor

--- example.py
+++ example.py
@@ -1,15 +1,8 @@
 def sweap_max(items) -> list:
     example = [];
     element = items[0]
-    max_pos = 0
-    d = 123
-    for x in range(len(items)):
-        if element <= items[x]:
-            element = items[x]
-            max_pos = x;
-            y = items[0]
-            items[0] = items[max_pos]
-            items[max_pos] = y
+    max_pos = items.index(max(items))
+    items[0] , items[max_pos]= items[max_pos], items[0]
     return items
 my_list = [ 2,3,4,5,6]
 print(sweap_max(my_list))
